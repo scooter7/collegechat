18666d71df13ccdd055d4422cdafb62a84430414
scooter7 <scooter.vineburgh@gmail.com>
2024-07-20 20:51:24 -0500

    Update collegechat.py

--- collegechat.py
+++ collegechat.py
@@ -138,10 +138,11 @@ with st.form(key="user_details_form"):
 
     selected_schools = []
     if 'relevant_schools' in st.session_state and st.session_state['relevant_schools']:
-        selected_schools = st.multiselect(
-            "Select the schools you are interested in:",
-            st.session_state['relevant_schools']
-        )
+        st.write("Select the schools you are interested in:")
+        for school in st.session_state['relevant_schools']:
+            selected = st.checkbox(school, key=school)
+            if selected:
+                selected_schools.append(school)
 
     submit_button = st.form_submit_button("Submit")
 
